f547614b0fba1296c24100d3c4dddaa2106de11a
Pedro Candido <pedrohcn02@gmail.com>
2021-10-13 11:25:06 -0300

    scripts6

---
+++ Fundamentos/Bloco-4-Intro-Javascript-LogicaProg/Dia-4.1/scripts6.js
@@ -0,0 +1,13 @@
+let weekDay = "sexta-feira";
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+}
+
+else if (weekDay === "sexta-feira") {
+    console.log("SEXTOU FAMÍLIA! (com aprendizados)")
+}
+
+else {
+    console.log("FINALMENTE, descanso merecido UwU")
+}
