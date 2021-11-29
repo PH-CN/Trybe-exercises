40d040467d4c6dfe50e004de4959fa335b052a3f
Pedro Candido <pedrohcn02@gmail.com>
2021-11-29 20:25:28 -0300

    testes do dia 9.1 partindo pros exercicios

---
+++ 1-Fundamentals/Block-9-JavaSCript-Async-Tests/9.1/test4.js
@@ -0,0 +1,15 @@
+const getUser = (callback) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  return callback(userToReturn);
+};
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+
+console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
+console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
