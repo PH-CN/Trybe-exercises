b76d55cbb8861e9de9f18983a2072bfeaf6a76f3
Pedro Candido <pedrohcn02@gmail.com>
2022-05-05 09:16:38 -0300

    a

--- 3-Back-end/Block-23-NodeJs-Service-Layer-Rest-Restful-Architecture/23.2/hello-msc/index.js
+++ 3-Back-end/Block-23-NodeJs-Service-Layer-Rest-Restful-Architecture/23.2/hello-msc/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const rescue = require('express-rescue');
+const errorMiddleware = require('./middlewares/error');
 const Author = require('./controllers/Authors');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/authors', Author.getAll);
+app.get('/authors', rescue(Author.getAll));
 
-app.get('/authors/:id', Author.findById);
+app.get('/authors/:id', rescue(Author.findById));
 
-app.post('/authors', Author.createAuthor);
+app.post('/authors', rescue(Author.createAuthor));
+
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
 
