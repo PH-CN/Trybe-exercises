cb4602fc7ecc66ed7b1a0f6d2199830ad6ae266a
Pedro Candido <pedrohcn02@gmail.com>
2022-05-19 12:12:26 -0300

    dia 24.2 finalizado

--- 3-Back-end/Block-24-NodeJs-ORM-Authentication/24.2/fixation-1/index.js
+++ 3-Back-end/Block-24-NodeJs-ORM-Authentication/24.2/fixation-1/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Address, Employee } = require('./models');
+const { Address, Employee, Book, User } = require('./models');
 
 const app = express();
 
@@ -19,7 +19,7 @@ app.get('/employees', async (_req, res) => {
 app.get('/employees/:id', async (req, res) => {
   try {
     const { id } = req.params;
-const employee = await Employee.findOne({ where: { id } });
+    const employee = await Employee.findOne({ where: { id } });
 
     if (!employee)
       return res.status(404).json({ message: 'Funcionário não encontrado' });
@@ -30,6 +30,24 @@ const employee = await Employee.findOne({ where: { id } });
        }
 
     return res.status(200).json(employee);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
+app.get('/usersbooks/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({
+      where: { userId: id },
+      include: [{ model: Book, as: 'books', through: { attributes: [] } }],
+    });
+
+    if (!user)
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+
+    return res.status(200).json(user);
   } catch (e) {
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
